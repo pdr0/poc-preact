229c18aac12454452f224d25ca6bcaa3d9154790
pedro selas <yo@pedroselas.com>
2019-10-17 13:19:52 +0200

    Update test-unit.sh

--- .bin/test-unit.sh
+++ .bin/test-unit.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-if [[ $(basename $PWD) == "betslip-mono" ]]; then
-    export NODE_ICU_DATA="$PWD/node_modules/full-icu"
-else
-    export NODE_ICU_DATA="$PWD/../../../node_modules/full-icu" # dangerous here, hardcoded path
-fi
 
-jest --config="./node_modules/@betslip/test/jest.config.js" --rootDir="./" $@
+jest --config="./node_modules/@wherever/test/jest.config.js" --rootDir="./" $@
